3c581d4cc49317eee379ccd599f015237079ef57
HwangYHa <yhwang7201@gmail.com>
2021-08-29 23:50:55 +0900

    Create Customer.js

---
+++ src/components/Customer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <CusomerProfile id={this.props.id} immage={this.props.image} name={this.props.name} />
+                <CusomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+            </div>
+        )
+
+    }
+}
+
+class CusomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CusomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
